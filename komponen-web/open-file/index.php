b550dd07cae6b71918b27495f7172304a7e54725
A Naive Dreamer <40853350+A-Naive-Dreamer@users.noreply.github.com>
2018-07-06 20:17:45 +0700

    Create index.php

---
+++ komponen-web/open-file/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en-us">
+	<head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>
+	        Windows Manager
+	</title>
+        <link rel="stylesheet" type="text/css" href="/3a-os/style/style1.css" />
+	<link rel="stylesheet" type="text/css" href="/3a-os/applications/file-manager/style.css" />
+    </head>
+    <body onload="setFileManager()">
+        <?php
+            require "file-manager.xml";
+        ?>
+	    <script src="/3a-os/javascript/function1.js">
+	    </script>
+	    <script src="/3a-os/applications/file-manager/function.js">
+	    </script>
+    </body>
+</html>
