9ef3df12dba5ff90a62b214fc751af9df23fd6f9
A Naive Dreamer <40853350+A-Naive-Dreamer@users.noreply.github.com>
2018-07-06 23:05:02 +0700

    Update index.php

--- komponen-web/open-file/index.php
+++ komponen-web/open-file/index.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>
-	        Windows Manager
+	        File Manager
 	</title>
         <link rel="stylesheet" type="text/css" href="/3a-os/style/style1.css" />
 	<link rel="stylesheet" type="text/css" href="/3a-os/applications/file-manager/style.css" />
